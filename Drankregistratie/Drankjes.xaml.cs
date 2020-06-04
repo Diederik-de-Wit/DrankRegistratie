0a976ed6f64b505b244fc1d3ce23bde20c1f94c4
Diederik de Wit <d276022@edu.rocwb.nl>
2020-06-04 12:29:52 +0200

    2e commit

---
+++ Drankregistratie/Drankjes.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drankregistratie
+{
+    /// <summary>
+    /// Interaction logic for Drankjes.xaml
+    /// </summary>
+    public partial class Drankjes : UserControl
+    {
+        DataClasses1DataContext db;
+        private dranken hetItem;
+        public Drankjes(dranken hetItem, DataClasses1DataContext db)
+        {
+            this.db = db;
+            this.hetItem = hetItem;
+            InitializeComponent();
+            txtNaam.Text = hetItem.NAAM;
+            txtSoort.Text = hetItem.SOORT;
+            txtPrijs.Text = hetItem.PRIJS.ToString();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            hetItem.NAAM = txtNaam.Text;
+            hetItem.SOORT = txtSoort.Text;
+            hetItem.PRIJS = Convert.ToDecimal(txtPrijs.Text);
+            db.SubmitChanges();
+        }
+
+        private void btnSave1_Click(object sender, RoutedEventArgs e)
+        {
+            dranken deDrank = new dranken();
+            deDrank.NAAM = txtNaam.Text;
+            deDrank.SOORT = txtSoort.Text;
+            deDrank.PRIJS = Convert.ToDecimal(txtPrijs.Text);
+            db.drankens.InsertOnSubmit(deDrank);
+            db.SubmitChanges();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int id = 0;
+            if (Int32.TryParse(hetItem.ID.ToString(), out id))
+            {
+                var hetProduct = (from dranken in db.drankens
+                                  where dranken.ID == id
+                                  select dranken).Single();
+
+                db.drankens.DeleteOnSubmit(hetProduct);
+                db.SubmitChanges();
+            }
+        }
+    }
+}
---
+++ Drankregistratie/UserControl1.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drankregistratie
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public UserControl1()
+        {
+            InitializeComponent();
+            drankenDG.ItemsSource = db.drankens.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as Panel).Children.Remove(this);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (dranken)drankenDG.SelectedItem;
+            drankjesDG.Children.Clear();
+            Drankjes ep = new Drankjes(item, db);
+            drankjesDG.Children.Add(ep);
+        }
+
+        private void btnSave1_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (dranken)drankenDG.SelectedItem;
+            drankjesDG.Children.Clear();
+            Drankjes eps = new Drankjes(item, db);
+            drankjesDG.Children.Add(eps);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (dranken)drankenDG.SelectedItem;
+            drankjesDG.Children.Clear();
+            Drankjes eps = new Drankjes(item, db);
+            drankjesDG.Children.Add(eps);
+        }
+
+        private void btnZoek_Click(object sender, RoutedEventArgs e)
+        {
+            string sWaarde = txtZoek.Text;
+            var lijst = db.drankens.Where(p => Convert.ToString(p.ID).Contains(sWaarde));
+            drankenDG.ItemsSource = lijst;
+        }
+    }
+}
